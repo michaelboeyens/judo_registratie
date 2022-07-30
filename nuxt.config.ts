95af18bb440a9b9eb7eba15fa5affd814e9b4884
Michaël Boeyens <michael.boeyens@gmail.com>
2022-07-30 16:54:36 +0200

    changes nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,12 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "Inschrijving Koninklijke Judoclub Bazel",
+      htmlAttrs: { lang: "nl" },
+    },
+  },
   css: ["normalize.css", "@formkit/themes/genesis", "~/assets/scss/main.scss"],
   modules: ["@formkit/nuxt"],
   telemetry: false,
