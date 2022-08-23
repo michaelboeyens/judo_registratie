994446d6260e41369dced7e81bf2937fe5ff7bd5
Michaël Boeyens <michael.boeyens@gmail.com>
2022-08-23 18:46:52 +0200

    bugfix

--- server/api/form.post.ts
+++ server/api/form.post.ts
@@ -22,7 +22,7 @@ export default defineEventHandler(async (event) => {
 
     // check and transform user vars
     email = escape(trim(email));
-    email2 = escape(trim(email2));
+    email2 = escape(trim(email2 || ""));
     phone = escape(trim(phone));
     streetName = escape(trim(streetName));
     streetNumber = escape(trim(streetNumber));
@@ -47,85 +47,85 @@ export default defineEventHandler(async (event) => {
 
     if (!isEmail(email) || !isEmail(email2)) {
       return { sendMail: false };
-    }
+    } else {
+      // create new returnObj with validated values
+      const returnObj = {
+        email,
+        phone,
+        streetName,
+        streetNumber,
+        postalCode,
+        city,
+        registration,
+        firstName,
+        lastName,
+        birthDate,
+        other,
+        photoAgreement,
+        gJudoka: additional.gJudoka,
+        thirdMember: additional.thirdMember,
+      };
 
-    // create new returnObj with validated values
-    const returnObj = {
-      email,
-      phone,
-      streetName,
-      streetNumber,
-      postalCode,
-      city,
-      registration,
-      firstName,
-      lastName,
-      birthDate,
-      other,
-      photoAgreement,
-      gJudoka: additional.gJudoka,
-      thirdMember: additional.thirdMember,
-    };
+      console.log("user input: ", JSON.parse(JSON.stringify(returnObj)));
 
-    console.log("user input: ", JSON.parse(JSON.stringify(returnObj)));
+      // get env vars
+      const config = useRuntimeConfig();
 
-    // get env vars
-    const config = useRuntimeConfig();
+      // define connection to mailserver
+      const transporter = createTransport({
+        host: config.emailHost,
+        port: 465,
+        secure: true,
+        auth: {
+          user: config.emailServername,
+          pass: config.emailPassword,
+        },
+      });
 
-    // define connection to mailserver
-    const transporter = createTransport({
-      host: config.emailHost,
-      port: 465,
-      secure: true,
-      auth: {
-        user: config.emailServername,
-        pass: config.emailPassword,
-      },
-    });
+      // message to admin
+      const mailMessage = {
+        from: config.emailSender,
+        to: config.emailRecipient,
+        subject: `Inschrijving KJC Bazel: ${firstName} ${lastName}`,
+        text: JSON.stringify(returnObj),
+        html: `<p>${JSON.stringify(returnObj)}</p>`,
+      };
 
-    // message to admin
-    const mailMessage = {
-      from: config.emailSender,
-      to: config.emailRecipient,
-      subject: `Inschrijving KJC Bazel: ${firstName} ${lastName}`,
-      text: JSON.stringify(returnObj),
-      html: `<p>${JSON.stringify(returnObj)}</p>`,
-    };
-
-    // message to client
-    const clientMailMessage = {
-      from: config.emailSender,
-      to: email,
-      subject: `Bevesting inschrijving ${firstName} ${lastName} bij Koninklijke Judoclub Bazel`,
-      text: `Bedankt voor uw inschrijving,\n
+      // message to client
+      const clientMailMessage = {
+        from: config.emailSender,
+        to: email,
+        subject: `Bevesting inschrijving ${firstName} ${lastName} bij Koninklijke Judoclub Bazel`,
+        text: `Bedankt voor uw inschrijving,\n
 Wij hebben ze goed ontvangen, en gaan hiermee verder aan de slag.\n
 \n
 Met vriendelijke groeten,\n
 Koninklijke Judoclub Bazel`,
-      html: `<p>Beste ${firstName},</p>
+        html: `<p>Beste ${firstName},</p>
       <p>Bedankt voor uw inschrijving,<br />
       Wij hebben ze goed ontvangen, en gaan hiermee verder aan de slag.</p>
       <br />
       <p>Met vriendelijke groeten,<br />
       Koninklijke Judoclub Bazel</p>`,
-    };
+      };
 
-    const response = await transporter.sendMail(mailMessage);
-    let clientResponse: SentMessageInfo | null = null;
-    if (response.accepted.length > 0) {
-      clientResponse = await transporter.sendMail(clientMailMessage);
+      const response = await transporter.sendMail(mailMessage);
+      let clientResponse: SentMessageInfo | null = null;
+      if (response.accepted.length > 0) {
+        clientResponse = await transporter.sendMail(clientMailMessage);
+      }
+
+      if (email2) {
+        clientMailMessage.to = email2;
+        clientResponse = await transporter.sendMail(clientMailMessage);
+      }
+
+      return {
+        sendMail: true,
+        emailResponse: response,
+        clientEmailResponse: clientResponse,
+      };
     }
-
-    if (email2) {
-      clientMailMessage.to = email2;
-      clientResponse = await transporter.sendMail(clientMailMessage);
-    }
-
-    return {
-      sendMail: returnObj,
-      emailResponse: response,
-      clientEmailResponse: clientResponse,
-    };
   } catch (err) {
     console.error(err);
     return { sendMail: false };
